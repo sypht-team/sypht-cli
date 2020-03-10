c318636e0093f74a3ee69089570e99ad84051ab7
NOTYWTR <liuao1207@gmail.com>
2020-03-10 14:10:04 +1100

    update build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,12 @@ set -e
 rm -rf build
 mkdir build
 
-go build *.go
+go build -o sypht-cli *.go
 
 GOOS=windows GOARCH=amd64 go build -o sypht-cli.exe *.go
+
+cp config.json build/
+mv sypht-cli* build/
+
+zip -r assets.zip build/
+mv assets.zip build/
