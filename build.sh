664d681a33a33354b89b939ad4ca0df5446b6861
NOTYWTR <liuao1207@gmail.com>
2020-03-11 11:31:45 +1100

    update build.sh

--- build.sh
+++ build.sh
@@ -1,17 +1,31 @@
   
 #!/bin/bash
-
 set -e
+
+package_name="sypht-cli"
+
+platforms=("windows/amd64" "windows/386" "darwin/amd64")
 
 rm -rf build
 mkdir build
 
-go build -o sypht-cli *.go
+for platform in "${platforms[@]}"
+do
+    platform_split=(${platform//\// })
+    GOOS=${platform_split[0]}
+    GOARCH=${platform_split[1]}
+    output_name=$package_name'-'$GOOS'-'$GOARCH
+    zip_name=$output_name'.zip'
+    if [ $GOOS = "windows" ]; then
+        output_name+='.exe'
+    fi
 
-GOOS=windows GOARCH=amd64 go build -o sypht-cli.exe *.go
+    env GOOS=$GOOS GOARCH=$GOARCH go build -o $output_name $package
+    if [ $? -ne 0 ]; then
+        echo 'An error has occurred! Aborting the script execution...'
+        exit 1
+    fi
+    zip $zip_name $output_name config.json
+    mv $zip_name build/
+done
 
-cp config.json build/
-mv sypht-cli* build/
-
-zip -r assets.zip build/
-mv assets.zip build/
